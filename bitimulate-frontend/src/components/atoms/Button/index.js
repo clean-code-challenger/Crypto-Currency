d4da888d01e51c654c677c90a05faa3166bdadc1
RRR.tg <davidruizy1983@yandex.com>
2017-11-17 16:38:45 +0800

    5 day
    Front-end Project construction

---
+++ bitimulate-frontend/src/components/atoms/Button/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './style.scss';
+
+class Button extends Component {
+  render () {
+    return (
+      <div className={style.button}>
+        Button
+      </div>
+    )
+  }
+}
+
+export default Button
